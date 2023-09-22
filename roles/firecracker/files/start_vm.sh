7e66a462bcf1d52f5854e081459bfa94a03c9a79
patrickkenney9801 <patrickkenney9801@gmail.com>
2023-09-22 15:12:45 -0500

    feat: Add various dnf packages for observability and vms

--- roles/firecracker/files/start_vm.sh
+++ roles/firecracker/files/start_vm.sh
@@ -84,3 +84,4 @@ echo "For internet access inside the vm run:"
 echo "ip addr add 172.16.0.2/24 dev eth0"
 echo "ip link set eth0 up"
 echo "ip route add default via 172.16.0.1 dev eth0"
+echo 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
